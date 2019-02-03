c8ae33e5890e2d2da5ab8b3402d92603c9c0bfbd
Thomas Lahore <tom.lahore@gmail.com>
2019-02-02 17:42:12 -0800

    Better handling of backup files

--- server/app.js
+++ server/app.js
@@ -7,7 +7,12 @@ const port = process.env.PORT || 3000;
 const fs = require('fs');
 const bodyParser = require('body-parser');
 
-const $model = require('../js/model/model').$model;
+//const $model = require('../js/model/model').$model;
+
+let save_dir = 'saved-data/';
+let backup_dir = save_dir+'backups/'
+
+let MAX_BACKUPS = 10;
 
 let db = null;
 /*
@@ -37,8 +42,9 @@ app.route('/items').get((req, res) => {
 	console.log('');
 	console.log('-------------------------------------');
 	console.log('get all items');
+	//TODO: handle not finding file
 	let t1 = Date.now();
-	fs.readFile('items.txt', function read(err, data) {
+	fs.readFile(save_dir+'items.txt', function read(err, data) {
 	    if (err) {
 	        throw err; //TODO: handle this
 	    }
@@ -55,17 +61,66 @@ app.route('/items').post((req, res) => {
 	let items = req.body;
 	console.log('\tlength of items = ' + items.length);
 	let t1 = Date.now();
-	fs.writeFile('items.txt', JSON.stringify(items), (err) => {  
-	    // throws an error, you could also catch it here
+	let items_as_string = JSON.stringify(items);
+	
+    if (!fs.existsSync(save_dir)){
+	    fs.mkdirSync(save_dir);
+	    console.log('created saved-data directory');
+	}
+
+	fs.writeFile(save_dir+'items.txt', items_as_string, (err) => {  
 	    if (err) {
 	        throw err; //TODO: handle this
 	    }
 	    let t2 = Date.now();
-	    // success case, the file was saved
 	    console.log('items saved, took '+(t2-t1)+'ms');
+
+	    //handle backups
+	    if (!fs.existsSync(backup_dir)){
+		    fs.mkdirSync(backup_dir);
+		    console.log('created backup save directory');
+		}
+
+		fs.writeFile(backup_dir+'items.'+t2+'.txt', items_as_string, (err) => {
+			if (err) {
+	        	throw err; //TODO: handle this
+	    	}
+	    	let t3 = Date.now();
+	    	console.log('items backed up, took '+(t3-t2)+'ms');
+
+	    	fs.readdir(backup_dir, function(err, files) {
+	    		if (err) {
+	    			throw err;
+	    		}
+	    		console.log('\t' + files.length + ' backup versions (max = '+MAX_BACKUPS+')');
+
+	    		let out = [];
+	    		files.forEach(function(file) {
+			        var stats = fs.statSync(backup_dir+file);
+			        if(stats.isFile()) {
+			            out.push({"file":file, "mtime": stats.mtime.getTime()});
+			        }
+			    });
+			    out.sort(function(a,b) {
+			        return b.mtime - a.mtime;
+			    });
+			    for (let i = 0; i < out.length; i++) {
+			    	let f = out[i];
+			    	if (i == 0) {
+			    		console.log('\t\tmost recent: ' + f.file);
+			    	}
+			    	if (i >= MAX_BACKUPS) {
+			    		console.log('\t\tremoving:    ' + f.file);
+			    		fs.unlinkSync(backup_dir+f.file);
+			    	}
+			    }
+			    let t4 = Date.now();
+			    console.log('\tmanaging backups took '+(t4-t3)+'ms');
+	    	});
+	    });
 	});
 
-  	res.json({"message":"POST okay ("+items.length+" items)"});
+  	res.json({"message":"POST okay ("+items.length+" items)"}); //TODO: not 'okay' until completed with backups
 });
 
 app.route('/items/:itemId').post((req, res) => {
@@ -89,7 +144,7 @@ app.get('/', (req, res) => {
 */
 
 const server = app.listen(port, () => {
-	console.log(`Listening on port ${port}!`)
+	console.log(`Listening on port ${port}`);
 	/*
 	db = new sqlite3.Database(DB_PATH, (err) => {
 		if (err) {
