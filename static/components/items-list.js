5f57fc31d0a5bf3cba78661799cbb8ff47bbc7e7
Thomas Lahore <tom.lahore@gmail.com>
2023-02-14 17:44:49 -0800

    added logic for applying styles from reserved tags

--- static/components/items-list.js
+++ static/components/items-list.js
@@ -1,45 +1,85 @@
-"use strict";
-
+'use strict';
 
 class ItemsList extends HTMLElement {
 
-  constructor() {
-    super();
-    this.my_id = null;
-  }
+    constructor() {
+        super();
+        this.my_id = null;
+    }
 
-  render(items) {
-      let t1 = Date.now();
-      if (items === null) {
-          this.innerHTML = `<div id="${this.my_id}">Items go here</div>`;
-      }
-      else {
-            //TODO move css to separate file
+    applyClasses(tags) {
+        let classes = [];
+        debugger;
+        if (tags.includes('@heading')) {
+            classes.push('tag-heading');
+        }
+        if (tags.includes('@strikethrough')) {
+            classes.push('tag-strikethrough');
+        }
+        if (tags.includes('@bold')) {
+            classes.push('tag-bold');
+        }
+        if (tags.includes('@italic')) {
+            classes.push('tag-italic');
+        }
+        if (tags.includes('@monospace')) {
+            classes.push('tag-monospace');
+        }
+        if (tags.includes('@password')) {
+            classes.push('tag-password');
+        }
+        return classes;
+    }
+
+    renderItem(item) {
+        let content = `<div class="item" id="${item.id}">`;
+        let grid_row = 1;
+        let list_mode = false;
+        for (let subitem of item.subitems) {
+            let tags = subitem.tags.split(' ');
+            console.log(`$tags: ${tags}`);
+            let classes = this.applyClasses(tags);
+            if (list_mode) {
+                let column_start = subitem.indent * 2 + 4;  // 1 based and give room for the bullet and expand arrow
+                content += `<div class="subitem-lhs1" style="grid-row: ${grid_row}; grid-column-start: ${column_start - 3};"> </div>`;
+                content += `<div class="subitem-lhs2" style="grid-row: ${grid_row}; grid-column-start: ${column_start - 2};"> </div>`;
+                content += `<div class="subitem-lhs3" style="grid-row: ${grid_row}; grid-column-start: ${column_start - 1};"> </div>`;
+                content += `<div class="subitem ${classes.join(' ')}" style="rid-row: ${grid_row}; grid-column-start: ${column_start};">${subitem.data}</div>`;
+            }
+            else {
+                let column_start = subitem.indent * 2 + 2;  // 1 based and give room for the bullet and expand arrow
+                content += `<div class="subitem-lhs1" style="grid-row: ${grid_row}; grid-column-start: ${column_start - 1};"> </div>`;
+                content += `<div class="subitem ${classes.join(' ')}" style="grid-row: ${grid_row}; grid-column-start: ${column_start};">${subitem.data}</div>`;
+            }
+            grid_row++;
+        }
+        content += '</div>';
+        return content;
+    }
+
+    render(items) {
+        let t1 = Date.now();
+        if (items === null) {
+            this.innerHTML = `<div id="${this.my_id}">Items go here</div>`;
+        } else {
             let content = '<div id="${this.my_id}" class="items-list">';
             for (let item of items) {
-                content += `<div class="item" id="${item.id}">`;
-                let grid_row = 1;
-                for (let subitem of item.subitems) {
-                    let column_start = subitem.indent + 1;  // +1 because grid-column-start is 1-indexed
-                    content += `<div class="subitem" style="grid-row: ${grid_row}; grid-column-start: ${column_start};">${subitem.data}</div>`;
-                    grid_row++;
-                }
-                content += '</div>';
+                content += this.renderItem(item);
             }
             content += '</div>';
-          this.innerHTML = content;
-      }
-      let t2 = Date.now();
-      console.log('rendered items-list in ' + (t2 - t1) + 'ms');
-  }
+            this.innerHTML = content;
+        }
+        let t2 = Date.now();
+        console.log('rendered items-list in ' + (t2 - t1) + 'ms');
+    }
 
-  connectedCallback() {
-    this.my_id = this.getAttribute('id');
-    PubSub.subscribe('search.results', (msg, items) => {
-        this.render(items);
-    });
-    this.render(null);
-  }
+    connectedCallback() {
+        this.my_id = this.getAttribute('id');
+        PubSub.subscribe('search.results', (msg, items) => {
+            this.render(items);
+        });
+        this.render(null);
+    }
 
     disconnectedCallback() {
         //TODO remove event listeners
--- static/components/search-bar.js
+++ static/components/search-bar.js
@@ -1,17 +1,15 @@
-"use strict";
-
+'use strict';
 
 class SearchBar extends HTMLElement {
 
-  constructor() {
-    super();
-
-    this.INTERVAL = 50;
-    this.lastValueChecked = null;
-    this.currentValue = '';
-    this.currentParse = null;
-    this.my_id = null;
-  }
+    constructor() {
+        super();
+        this.INTERVAL = 50;
+        this.lastValueChecked = null;
+        this.currentValue = '';
+        this.currentParse = null;
+        this.my_id = null;
+    }
 
     checkForUpdatedSearch() {
         if (this.lastValueChecked === this.currentValue) {
@@ -21,125 +19,124 @@ class SearchBar extends HTMLElement {
         if (this.currentParse !== null) {
             PubSub.publish('search.updated', this.currentParse);
         }
-  }
+    }
 
-  render() {
-    this.innerHTML = `
-      <style></style>
-      
-      <input id="${this.my_id}" type="text" placeholder="search" spellcheck="false" size="64"/>
-    `;
-      this.querySelector('input').addEventListener('input', () => {
-        this.currentValue = this.querySelector('input').value;
+    render() {
+        this.innerHTML = `
+            <style></style>
+            <input id="${this.my_id}" type="text" placeholder="search" spellcheck="false" size="64"/>
+        `;
+        this.querySelector('input').addEventListener('input', () => {
+            this.currentValue = this.querySelector('input').value;
+            this.currentParse = this.parseSearch(this.currentValue);
+            if (this.currentParse === null) {
+                this.querySelector('input').style.backgroundColor = 'red';
+            } else {
+                this.querySelector('input').style.backgroundColor = 'white';
+            }
+        });
+    }
+
+    connectedCallback() {
+        this.my_id = this.getAttribute('id');
         this.currentParse = this.parseSearch(this.currentValue);
-        if (this.currentParse === null) {
-            this.querySelector('input').style.backgroundColor = 'red';
-        }
-        else {
-            this.querySelector('input').style.backgroundColor = 'white';
-        }
-      });
-  }
-
-  connectedCallback() {
-    this.my_id = this.getAttribute('id');
-    this.currentParse = this.parseSearch(this.currentValue);
-    this.intervalID = setInterval(this.checkForUpdatedSearch.bind(this), this.INTERVAL);
-    this.render();
-  }
+        this.intervalID = setInterval(this.checkForUpdatedSearch.bind(this),
+            this.INTERVAL);
+        this.render();
+    }
 
     disconnectedCallback() {
         clearInterval(this.intervalID);
         //TODO remove event listeners
     }
 
-  parseSearch(search) {
+    parseSearch(search) {
 
-    let parsedSearch = {
-        tags: [],
-        negated_tags: [],
-        texts: [],
-        negated_texts: [],
-        partial_tag: null,
-        negated_partial_tag: null,
-        partial_text: null,
-        negated_partial_text: null
-    }
+        let parsedSearch = {
+            tags: [],
+            negated_tags: [],
+            texts: [],
+            negated_texts: [],
+            partial_tag: null,
+            negated_partial_tag: null,
+            partial_text: null,
+            negated_partial_text: null,
+        };
 
-    if (search.trim() === '') {
+        if (search.trim() === '') {
+            return parsedSearch;
+        }
+
+        const tag = /^\s*([^\s,-;`\\"][^\s,;`\\"]*)\s+/;
+        const neg_tag = /^\s*-([^\s,-;`\\"][^\s,;`\\"]*)\s+/;
+        const partial_tag = /^\s*([^\s,-;`\\"][^\s,;`\\"]*)$/;
+        const partial_neg_tag = /^\s*-([^\s,-;`\\"][^\s,;`\\"]*)$/;
+        const text = /^\s*"([^"\\]+)"\s*/;
+        const neg_text = /^\s*-"([^"\\]+)"\s*/;
+        const partial_text = /^\s*"([^"\\]+)$/;
+        const partial_neg_text = /^\s*-"([^"\\]+)$/;
+        const ignore_end = /^\s*-?"?$/;
+
+        let temp = search;
+        while (temp.trim() !== '') {
+            let match = temp.match(tag);
+            if (match) {
+                parsedSearch.tags.push(match[1]);
+                temp = temp.substring(match[0].length);
+                continue;
+            }
+            match = temp.match(neg_tag);
+            if (match) {
+                parsedSearch.negated_tags.push(match[1]);
+                temp = temp.substring(match[0].length);
+                continue;
+            }
+            match = temp.match(partial_tag);
+            if (match) {
+                parsedSearch.partial_tag = match[1];
+                temp = temp.substring(match[0].length);
+                continue;
+            }
+            match = temp.match(partial_neg_tag);
+            if (match) {
+                parsedSearch.negated_partial_tag = match[1];
+                temp = temp.substring(match[0].length);
+                continue;
+            }
+            match = temp.match(text);
+            if (match) {
+                parsedSearch.texts.push(match[1]);
+                temp = temp.substring(match[0].length);
+                continue;
+            }
+            match = temp.match(neg_text);
+            if (match) {
+                parsedSearch.negated_texts.push(match[1]);
+                temp = temp.substring(match[0].length);
+                continue;
+            }
+            match = temp.match(partial_text);
+            if (match) {
+                parsedSearch.partial_text = match[1];
+                temp = temp.substring(match[0].length);
+                continue;
+            }
+            match = temp.match(partial_neg_text);
+            if (match) {
+                parsedSearch.negated_partial_text = match[1];
+                temp = temp.substring(match[0].length);
+                continue;
+            }
+            match = temp.match(ignore_end);
+            if (match) {
+                temp = temp.substring(match[0].length);
+                continue;
+            }
+            console.warn(`could not parse search: "${temp}"`);
+            return null;
+        }
         return parsedSearch;
     }
-
-    const tag = /^\s*([^\s,-;`\\"][^\s,;`\\"]*)\s+/;
-    const neg_tag = /^\s*-([^\s,-;`\\"][^\s,;`\\"]*)\s+/;
-    const partial_tag = /^\s*([^\s,-;`\\"][^\s,;`\\"]*)$/;
-    const partial_neg_tag = /^\s*-([^\s,-;`\\"][^\s,;`\\"]*)$/;
-    const text = /^\s*"([^"\\]+)"\s*/;
-    const neg_text = /^\s*-"([^"\\]+)"\s*/;
-    const partial_text = /^\s*"([^"\\]+)$/;
-    const partial_neg_text = /^\s*-"([^"\\]+)$/;
-    const ignore_end = /^\s*-?"?$/;
-
-    let temp = search;
-    while (temp.trim() !== '') {
-        let match = temp.match(tag);
-        if (match) {
-            parsedSearch.tags.push(match[1]);
-            temp = temp.substring(match[0].length);
-            continue;
-        }
-        match = temp.match(neg_tag);
-        if (match) {
-            parsedSearch.negated_tags.push(match[1]);
-            temp = temp.substring(match[0].length);
-            continue;
-        }
-        match = temp.match(partial_tag);
-        if (match) {
-            parsedSearch.partial_tag = match[1];
-            temp = temp.substring(match[0].length);
-            continue;
-        }
-        match = temp.match(partial_neg_tag);
-        if (match) {
-            parsedSearch.negated_partial_tag = match[1];
-            temp = temp.substring(match[0].length);
-            continue;
-        }
-        match = temp.match(text);
-        if (match) {
-            parsedSearch.texts.push(match[1]);
-            temp = temp.substring(match[0].length);
-            continue;
-        }
-        match = temp.match(neg_text);
-        if (match) {
-            parsedSearch.negated_texts.push(match[1]);
-            temp = temp.substring(match[0].length);
-            continue;
-        }
-        match = temp.match(partial_text);
-        if (match) {
-            parsedSearch.partial_text = match[1];
-            temp = temp.substring(match[0].length);
-            continue;
-        }
-        match = temp.match(partial_neg_text);
-        if (match) {
-            parsedSearch.negated_partial_text = match[1];
-            temp = temp.substring(match[0].length);
-            continue;
-        }
-        match = temp.match(ignore_end);
-        if (match) {
-            temp = temp.substring(match[0].length);
-            continue;
-        }
-        console.warn(`could not parse search: "${temp}"`);
-        return null;
-    }
-    return parsedSearch;
-  }
 
 }
 
