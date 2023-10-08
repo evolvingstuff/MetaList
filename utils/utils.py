259d047dc5f6efc65be3d41e36973b1e1c3fc38c
Thomas Lahore <tom.lahore@gmail.com>
2023-10-08 07:56:53 -0700

    fixed match logic to properly handle negatives as well

--- utils/utils.py
+++ utils/utils.py
@@ -62,25 +62,50 @@ def recalculate_item_ranks(cache):
 
 def propagate_matches(item):
     # TODO 2023.02.17 this could be more efficient (use a stack)
-    # asdfasdf _block
-    added_indices = set()
+
+    """
+    Stages:
+    1) propagate blocks to children
+    2) propagate matches to parents
+    3) propagate matches to children
+    """
+    blocked_indices = set()
     for i, subitem in enumerate(item['subitems']):
-        if '_match' in subitem:
-            indent_cursor = subitem['indent']
-            # propagate up
-            for j in range(i-1, -1, -1):
-                subitem2 = item['subitems'][j]
-                if subitem2['indent'] < indent_cursor:
-                    added_indices.add(j)
-                    indent_cursor = subitem2['indent']
-            # propagate down
+        if '_block' in subitem:
+            # 1) propagate blocks to children
             for j in range(i+1, len(item['subitems'])):
                 subitem2 = item['subitems'][j]
                 if subitem2['indent'] > subitem['indent']:
-                    added_indices.add(j)
+                    blocked_indices.add(j)
                 else:
                     break
-    for i in added_indices:
+    for i in blocked_indices:
+        if '_match' in item['subitems'][i]:
+            del item['subitems'][i]['_match']
+        item['subitems'][i]['_block'] = True
+
+    matched_indices = set()
+    for i, subitem in enumerate(item['subitems']):
+        if '_match' in subitem:
+            indent_cursor = subitem['indent']
+            # 2) propagate matches to parents
+            for j in range(i-1, -1, -1):
+                parent_subitem = item['subitems'][j]
+                if parent_subitem['indent'] < indent_cursor:
+                    if '_block' in parent_subitem:
+                        break
+                    matched_indices.add(j)
+                    indent_cursor = parent_subitem['indent']
+            # 3) propagate matches to children
+            for j in range(i+1, len(item['subitems'])):
+                child_subitem = item['subitems'][j]
+                if '_block' in child_subitem:
+                    break
+                if child_subitem['indent'] > subitem['indent']:
+                    matched_indices.add(j)
+                else:
+                    break
+    for i in matched_indices:
         item['subitems'][i]['_match'] = True
 
 
