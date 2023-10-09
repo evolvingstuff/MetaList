b4fef6c24a55feed1f10735933e27fe378580f6f
Thomas Lahore <tom.lahore@gmail.com>
2023-10-08 17:04:14 -0700

    rearranged logic of item filters

--- config/config.py
+++ config/config.py
@@ -1,6 +1,8 @@
 propagate_decorations = True
-use_partial_tag_matches_positive = True  # TODO: disable this eventually
+use_partial_tag_matches_positive = True  # TODO: disable this eventually, or somehow avoid problem of adding items with partial tags
 use_partial_tag_matches_negative = False
+use_partial_text_matches_positive = True
+use_partial_text_matches_negative = False
 db_path = 'metalist.2.0.db'  # TODO from root or somewhere else
 max_results = 50  # TODO need dynamic pagination
 always_add_to_global_top = True
--- utils/utils.py
+++ utils/utils.py
@@ -140,6 +140,9 @@ def filter_subitem_negative(subitem, search_filter: str) -> bool:
     subitem_text = subitem['_clean_text']
     subitem_tags = subitem['_tags']
     # TODO: for better efficiency, could order these by frequency, descending
+    for negated_tag in search_filter['negated_tags']:
+        if negated_tag.lower() in [t.lower() for t in subitem_tags]:
+            return True
     if search_filter['negated_partial_tag'] is not None:
         # TODO: 2023.09.21 apply use_partial_tag_matches logic
         for subitem_tag in subitem_tags:
@@ -149,15 +152,13 @@ def filter_subitem_negative(subitem, search_filter: str) -> bool:
             else:
                 if subitem_tag.lower() == search_filter['negated_partial_tag'].lower():
                     return True
-    if search_filter['negated_partial_text'] is not None and \
-            search_filter['negated_partial_text'].lower() in subitem_text:
-        return True
-    for negated_tag in search_filter['negated_tags']:
-        if negated_tag.lower() in [t.lower() for t in subitem_tags]:
-            return True
     for negated_text in search_filter['negated_texts']:
         if negated_text.lower() in subitem_text:
             return True
+    if use_partial_text_matches_negative and \
+            search_filter['negated_partial_text'] is not None and \
+            search_filter['negated_partial_text'].lower() in subitem_text:
+        return True
     return False
 
 
@@ -165,9 +166,9 @@ def filter_subitem_positive(subitem, search_filter: str) -> bool:
     subitem_text = subitem['_clean_text']
     subitem_tags = subitem['_tags']
     # TODO: for better efficiency, could order these by frequency, ascending
-    if search_filter['partial_text'] is not None and \
-            search_filter['partial_text'].lower() not in subitem_text:
-        return False
+    for required_tag in search_filter['tags']:
+        if required_tag.lower() not in [t.lower() for t in subitem_tags]:
+            return False
     if search_filter['partial_tag'] is not None:
         one_match = False
         for subitem_tag in subitem_tags:
@@ -181,12 +182,13 @@ def filter_subitem_positive(subitem, search_filter: str) -> bool:
                     break
         if not one_match:
             return False
-    for required_tag in search_filter['tags']:
-        if required_tag.lower() not in [t.lower() for t in subitem_tags]:
-            return False
     for required_text in search_filter['texts']:
         if required_text.lower() not in subitem_text:
             return False
+    if use_partial_text_matches_positive and \
+            search_filter['partial_text'] is not None and \
+            search_filter['partial_text'].lower() not in subitem_text:
+        return False
     return True
 
 
