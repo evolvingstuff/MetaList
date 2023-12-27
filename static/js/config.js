88c0e57bd39f6954463c1534b4ba82a2b3bd2e2f
Thomas Lahore <tom.lahore@gmail.com>
2023-12-26 18:36:25 -0800

    small cleanup

--- static/js/config.js
+++ static/js/config.js
@@ -5,6 +5,5 @@ export const paginationExpandBy = 50;
 export const checkPaginationMs = 250;
 export const hideImpliesTagByDefault = true;
 /* https://platform.openai.com/docs/models */
-export const openAiModel = 'gpt-3.5-turbo-16k';
-export const staticPrompt = null;
+export const openAiModel = 'gpt-4-1106-preview';
 export const promptInjectionPoint = 'start';
