6e4a89ec02724b8ecfa6e03c102f5b348aacb6c8
Thomas Lahore <tom.lahore@gmail.com>
2023-12-28 07:42:36 -0800

    added history compression

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from utils.tags_suggestions import calculate_tags_suggestions
 from utils.update_multiple_items import *
 from utils.update_single_item import *
 from utils.initialize import initialize_cache
-from utils.snapshots import Snapshots, SnapshotFragment, Snapshot
+from utils.snapshots import Snapshots, SnapshotFragment, Snapshot, compress_snapshots
 
 import utils
 
@@ -19,7 +19,7 @@ plugin = bottle_sqlite.Plugin(dbfile=db_path)
 app.install(plugin)
 
 cache = {}
-snapshots2 = Snapshots()
+snapshots = Snapshots()
 documents = None
 vectorizer = None
 tfidf_matrix = None
@@ -77,59 +77,63 @@ def get_lib(filepath):
 
 @app.post('/todo')
 def todo(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     utils.update_single_item.todo(context)
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, context.item_subitem_id)
-    snapshot = Snapshot('/todo', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/todo', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=context.item_subitem_id)
 
 
 @app.post('/done')
 def done(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     utils.update_single_item.done(context)
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, context.item_subitem_id)
-    snapshot = Snapshot('/done', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/done', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=context.item_subitem_id)
 
 
 @app.post('/expand')
 def expand(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     utils.update_single_item.expand(context)
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, context.item_subitem_id)
-    snapshot = Snapshot('/expand', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/expand', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=context.item_subitem_id)
 
 
 @app.post('/collapse')
 def collapse(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     utils.update_single_item.collapse(context)
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, context.item_subitem_id)
-    snapshot = Snapshot('/collapse', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/collapse', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=context.item_subitem_id)
 
 
 @app.post('/delete-subitem')
 def delete_subitem(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     # TODO: these should be two separate API calls
@@ -137,59 +141,64 @@ def delete_subitem(db):
         utils.update_multiple_items.remove_item(cache, context)
         filtered_items, reached_scroll_end = filter_items(cache, context, dirty_ranking=True)
         snap_post = SnapshotFragment(cache, None)
-        snapshot = Snapshot('/delete-subitem (item)', snap_pre, snap_post)
-        snapshots2.push(snapshot)
+        snapshot = Snapshot('/delete-subitem (item)', snap_pre, snap_post, context.item_subitem_id)
+        snapshots.push(snapshot)
+        compress_snapshots(snapshots)
         return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=None)
     else:
         utils.update_single_item.delete_subitem(context)
         filtered_items, reached_scroll_end = filter_items(cache, context)
         snap_post = SnapshotFragment(cache, None)
-        snapshot = Snapshot('/delete-subitem', snap_pre, snap_post)
-        snapshots2.push(snapshot)
+        snapshot = Snapshot('/delete-subitem', snap_pre, snap_post, context.item_subitem_id)
+        snapshots.push(snapshot)
+        compress_snapshots(snapshots)
         return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=None)
 
 
 @app.post('/update-subitem-content')
 def update_subitem_content(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     utils.update_single_item.update_subitem_content(context, cache)
     snap_post = SnapshotFragment(cache, context.item_subitem_id)
-    snapshot = Snapshot('/update-subitem-content', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/update-subitem-content', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return {}
 
 
 @app.post('/move-item-up')
 def move_item_up(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     utils.update_multiple_items.move_item_up(cache, context)
     filtered_items, reached_scroll_end = filter_items(cache, context, dirty_ranking=True)
     snap_post = SnapshotFragment(cache, context.item_subitem_id)
-    snapshot = Snapshot('/move-item-up', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/move-item-up', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=context.item_subitem_id)
 
 
 @app.post('/move-item-down')
 def move_item_down(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     utils.update_multiple_items.move_item_down(cache, context)
     filtered_items, reached_scroll_end = filter_items(cache, context, dirty_ranking=True)
     snap_post = SnapshotFragment(cache, context.item_subitem_id)
-    snapshot = Snapshot('/move-item-down', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/move-item-down', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=context.item_subitem_id)
 
 
 @app.post('/move-subitem-up')
 def move_subitem_up(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     try:
@@ -198,14 +207,15 @@ def move_subitem_up(db):
         return noop_response('illegal operation')
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, new_item_subitem_id)
-    snapshot = Snapshot('/move-subitem-up', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/move-subitem-up', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=new_item_subitem_id)
 
 
 @app.post('/move-subitem-down')
 def move_subitem_down(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     try:
@@ -214,14 +224,15 @@ def move_subitem_down(db):
         return noop_response('illegal operation')
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, new_item_subitem_id)
-    snapshot = Snapshot('/move-subitem-down', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/move-subitem-down', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=new_item_subitem_id)
 
 
 @app.post('/indent')
 def indent(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     try:
@@ -230,14 +241,15 @@ def indent(db):
         return noop_response('illegal operation')
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, context.item_subitem_id)
-    snapshot = Snapshot('/indent', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/indent', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=context.item_subitem_id)
 
 
 @app.post('/outdent')
 def outdent(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     try:
@@ -246,25 +258,27 @@ def outdent(db):
         return noop_response('illegal operation')
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, context.item_subitem_id)
-    snapshot = Snapshot('/outdent', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/outdent', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=context.item_subitem_id)
 
 
 @app.post('/search')
 def search(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     # TODO why remake context here?
     context = Context(context.app_state, search_text=context.search_text, search_filter=context.search_filter)
     filtered_items, reached_scroll_end = filter_items(cache, context, updated_search=True, dirty_ranking=True)
     snap_post = SnapshotFragment(cache, None)
-    snapshot = Snapshot('/search', snap_pre, snap_post)
+    snapshot = Snapshot('/search', snap_pre, snap_post, context.item_subitem_id)
     if reset_undo_stack_on_search:
-        snapshots2.reset()
+        snapshots.reset()
     else:
-        snapshots2.push(snapshot)
+        snapshots.push(snapshot)
+        compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=None)
 
 
@@ -290,49 +304,51 @@ def tags_suggestions(db):
     return result
 
 
-
 @app.post('/add-item-sibling')
 def add_item_sibling(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     new_item_subitem_id = utils.update_multiple_items.add_item_sibling(cache, context)
     filtered_items, reached_scroll_end = filter_items(cache, context, dirty_ranking=True)
     snap_post = SnapshotFragment(cache, new_item_subitem_id)
-    snapshot = Snapshot('/add-item-sibling', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/add-item-sibling', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=new_item_subitem_id)
 
 
 @app.post('/add-subitem-sibling')
 def add_subitem_sibling(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     new_item_subitem_id = utils.update_single_item.add_subitem_sibling(context)
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, new_item_subitem_id)
-    snapshot = Snapshot('/add-subitem-sibling', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/add-subitem-sibling', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=new_item_subitem_id)
 
 
 @app.post('/add-subitem-child')
 def add_subitem_child(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     new_item_subitem_id = utils.update_single_item.add_subitem_child(context)
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, new_item_subitem_id)
-    snapshot = Snapshot('/add-subitem-child', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/add-subitem-child', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=new_item_subitem_id)
 
 
 @app.post('/add-item-top')
 def add_item_top(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     # TODO: move this logic to client
@@ -341,42 +357,45 @@ def add_item_top(db):
     new_item_subitem_id = utils.update_multiple_items.add_item_top(cache, context)
     filtered_items, reached_scroll_end = filter_items(cache, context, dirty_ranking=True)
     snap_post = SnapshotFragment(cache, new_item_subitem_id)
-    snapshot = Snapshot('/add-item-top', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/add-item-top', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=new_item_subitem_id)
 
 
 @app.post('/paste-sibling')
 def paste_sibling(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     assert context.clipboard is not None, 'missing clipboard from request'
     new_item_subitem_id = utils.update_multiple_items.paste_sibling(cache, context)
     filtered_items, reached_scroll_end = filter_items(cache, context, dirty_ranking=True)
     snap_post = SnapshotFragment(cache, new_item_subitem_id)
-    snapshot = Snapshot('/paste-sibling', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/paste-sibling', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=new_item_subitem_id)
 
 
 @app.post('/paste-child')
 def paste_child(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     assert context.clipboard is not None, 'missing clipboard from request'
     new_item_subitem_id = utils.update_single_item.paste_child(context)
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, new_item_subitem_id)
-    snapshot = Snapshot('/paste-child', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/paste-child', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=new_item_subitem_id)
 
 
 @app.post('/pagination-update')
 def pagination_update(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     # no snapshot
     filtered_items, reached_scroll_end = filter_items(cache, context)
@@ -385,24 +404,25 @@ def pagination_update(db):
 
 @app.post('/update-tags')
 def update_tags(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     snap_pre = SnapshotFragment(cache, context.item_subitem_id)
     utils.update_single_item.update_tags(context)
     filtered_items, reached_scroll_end = filter_items(cache, context)
     snap_post = SnapshotFragment(cache, context.item_subitem_id)
-    snapshot = Snapshot('/update-tags', snap_pre, snap_post)
-    snapshots2.push(snapshot)
+    snapshot = Snapshot('/update-tags', snap_pre, snap_post, context.item_subitem_id)
+    snapshots.push(snapshot)
+    compress_snapshots(snapshots)
     return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=context.item_subitem_id)
 
 
 @app.post('/undo')
 def undo(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     # no snapshot
     try:
-        new_item_subitem_id = utils.update_multiple_items.undo2(snapshots2, cache)
+        new_item_subitem_id = utils.update_multiple_items.undo2(snapshots, cache)
         filtered_items, reached_scroll_end = filter_items(cache, context, dirty_ranking=True)  # TODO: just in case?
         return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=new_item_subitem_id)
     except Exception as e:
@@ -411,11 +431,11 @@ def undo(db):
 
 @app.post('/redo')
 def redo(db):
-    global cache, snapshots2
+    global cache, snapshots
     context = get_request_context(request, cache)
     # no snapshot
     try:
-        new_item_subitem_id = utils.update_multiple_items.redo2(snapshots2, cache)
+        new_item_subitem_id = utils.update_multiple_items.redo2(snapshots, cache)
         filtered_items, reached_scroll_end = filter_items(cache, context, dirty_ranking=True)  # TODO: just in case?
         return generic_response(filtered_items, reached_scroll_end, new_item_subitem_id=new_item_subitem_id)
     except Exception as e:
--- utils/snapshots.py
+++ utils/snapshots.py
@@ -10,10 +10,11 @@ class SnapshotFragment:
 
 
 class Snapshot:
-    def __init__(self, op_name: str, pre: SnapshotFragment, post: SnapshotFragment):
+    def __init__(self, op_name: str, pre: SnapshotFragment, post: SnapshotFragment, item_subitem_id: str):
         self.op_name = op_name
         self.pre = pre
         self.post = post
+        self.item_subitem_id = item_subitem_id
 
 
 class Snapshots:
@@ -67,3 +68,91 @@ class Snapshots:
                 print(f'\t[{s}] >> {snapshot.op_name}')
             else:
                 print(f'\t[{s}]    {snapshot.op_name}')
+
+
+def double_merge(op1, op2, snapshots):
+    if snapshots.stack_pointer < 1:
+        return False
+    a, b = snapshots.stack[snapshots.stack_pointer - 1], \
+        snapshots.stack[snapshots.stack_pointer]
+    if a.item_subitem_id != b.item_subitem_id:
+        return False
+    if a.op_name == op1 and b.op_name == op2:
+        print('=============================================')
+        print(f'match on {op1} {op2}')
+        print('pre:')
+        snapshots.show()
+        merged_snapshot = Snapshot(op1, a.pre, b.post, b.item_subitem_id)
+        # point at new desired location
+        snapshots.stack_pointer -= 1
+        # remove a and b
+        snapshots.stack.pop(snapshots.stack_pointer)
+        snapshots.stack.pop(snapshots.stack_pointer)
+        # add merged_snapshot
+        snapshots.stack.insert(snapshots.stack_pointer, merged_snapshot)
+        print('post')
+        snapshots.show()
+        print('=============================================')
+        return True
+    return False
+
+
+def triple_merge(op1, op2, op3, snapshots):
+    if snapshots.stack_pointer < 2:
+        return False
+    a, b, c = snapshots.stack[snapshots.stack_pointer - 2], \
+        snapshots.stack[snapshots.stack_pointer - 1], \
+        snapshots.stack[snapshots.stack_pointer]
+    if a.item_subitem_id != b.item_subitem_id or a.item_subitem_id != c.item_subitem_id:
+        return False
+    if a.op_name == op1 and b.op_name == op2 and c.op_name == op3:
+        print(f'match on {op1} {op2} {op3}')
+        merged_snapshot = Snapshot(op1, a.pre, c.post, c.item_subitem_id)
+        # point at new desired location
+        snapshots.stack_pointer -= 2
+        # remove a, b, and c
+        snapshots.stack.pop(snapshots.stack_pointer)
+        snapshots.stack.pop(snapshots.stack_pointer)
+        snapshots.stack.pop(snapshots.stack_pointer)
+        # add merged_snapshot
+        snapshots.stack.insert(snapshots.stack_pointer, merged_snapshot)
+        return True
+    return False
+
+
+double_patterns = [
+    ['/update-tags', '/update-tags'],
+    ['/update-subitem-content', '/update-subitem-content']
+]
+
+triple_patterns = [
+    ['/todo', '/done', '/todo'],
+    ['/done', '/todo', '/done'],
+    ['/expand', '/collapse', '/expand'],
+    ['/collapse', '/expand', '/collapse'],
+    ['/indent', '/outdent', '/indent'],
+    ['/outdent', '/indent', '/outdent'],
+    ['/move-item-up', '/move-item-down', '/move-item-up'],
+    ['/move-item-down', '/move-item-up', '/move-item-down'],
+    ['/move-subitem-up', '/move-subitem-down', '/move-subitem-up'],
+    ['/move-subitem-down', '/move-subitem-up', '/move-subitem-down']
+]
+
+
+def compress_snapshots(snapshots: Snapshots):
+    print('compress_snapshots()')
+    matches = 0
+    while True:
+        for pattern in double_patterns:
+            if double_merge(pattern[0], pattern[1], snapshots):
+                matches += 1
+                continue
+        for pattern in triple_patterns:
+            if triple_merge(pattern[0], pattern[1], pattern[2], snapshots):
+                matches += 1
+                continue
+        if matches == 0:
+            print('no compression matches found')
+        else:
+            snapshots.show()
+        return
