df018b071c4ca106435671a02622a2e6c2734609
Thomas Lahore <tom.lahore@gmail.com>
2023-12-31 06:29:25 -0800

    update setup.py info

--- setup.py
+++ setup.py
@@ -1,34 +1,36 @@
 from setuptools import setup
 
+
 def read_requirements():
     with open('requirements.txt') as req:
         return req.read().splitlines()
 
+
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
+
 
 setup(
     name='MetaList',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
-    description='A short description of your MetaList project',
+    author='Thomas Lahore',
+    author_email='tom.lahore@gmail.com',
+    description='A PKM web app integrated with LLMs',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/metalist',
+    url='https://github.com/evolvingstuff/metalist',
     install_requires=read_requirements(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    # packages=['metalist'],  # Manually specifying your package directory
     package_data={
         'metalist': ['static/**/*'],
     },
     entry_points={
         'console_scripts': [
-            'metalist=metalist.app:run_app',  # Points to the main function in metalist/app.py
+            'metalist=metalist.app:run_app',
         ],
     },
     python_requires='>=3.6',
